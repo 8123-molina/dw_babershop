a238ce1ef1e1239bf77801277d487772b5e0f645
Dhyone Molina <molinaprojetos@gmail.com>
2023-08-29 13:53:51 -0300

    add os forms ao  login e validações aos mesmo

--- lib/src/features/auth/login/login_page.dart
+++ lib/src/features/auth/login/login_page.dart
@@ -1,8 +1,26 @@
 import 'package:dw_barbershop/src/core/ui/constants.dart';
+import 'package:dw_barbershop/src/core/ui/helpers/form_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:validatorless/validatorless.dart';
 
-class LoginPage extends StatelessWidget {
+class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final formKey = GlobalKey<FormState>();
+  final emailEC = TextEditingController();
+  final passwordEC = TextEditingController();
+
+  @override
+  void dispose() {
+    emailEC.dispose();
+    passwordEC.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,79 +29,97 @@ class LoginPage extends StatelessWidget {
 
     return Scaffold(
       backgroundColor: Colors.black,
-      body: DecoratedBox(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-              image: AssetImage(ImageConstants.backgroudChair),
-              opacity: 0.3,
-              fit: BoxFit.cover),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(30.0),
-          child: CustomScrollView(slivers: [
-            SliverFillRemaining(
-                hasScrollBody: false,
-                child: Stack(
-                  alignment: Alignment.center,
-                  children: [
-                    Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Image.asset(ImageConstants.imageLogo),
-                        separete,
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text('Email'),
-                            hintText: 'E-mail',
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            labelStyle: TextStyle(color: Colors.black),
-                            hintStyle: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        separete,
-                        TextFormField(
-                          decoration: const InputDecoration(
-                            label: Text('Senha'),
-                            hintText: 'Senha',
-                            floatingLabelBehavior: FloatingLabelBehavior.never,
-                            labelStyle: TextStyle(color: Colors.black),
-                            hintStyle: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        separete,
-                        const Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Esqueceu a senha',
-                            style: TextStyle(
-                              color: ColorsConstants.brow,
-                              fontSize: 12,
+      body: Form(
+        key: formKey,
+        child: DecoratedBox(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage(ImageConstants.backgroudChair),
+                opacity: 0.3,
+                fit: BoxFit.cover),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: CustomScrollView(slivers: [
+              SliverFillRemaining(
+                  hasScrollBody: false,
+                  child: Stack(
+                    alignment: Alignment.center,
+                    children: [
+                      Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(ImageConstants.imageLogo),
+                          separete,
+                          TextFormField(
+                            onTapOutside:(_) => context.unfocus(),
+                            validator: Validatorless.multiple([
+                              Validatorless.required('Email obrigatório'),
+                              Validatorless.email('Email inválido'),
+                            ]),
+                            controller: emailEC,
+                            decoration: const InputDecoration(
+                              label: Text('Email'),
+                              hintText: 'E-mail',
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
                             ),
                           ),
-                        ),
-                        separete,
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            minimumSize: Size.fromHeight(screenSize.height * 0.07),
+                          separete,
+                          TextFormField(
+                            onTapOutside: (_) => context.unfocus(),
+                            validator: Validatorless.multiple([
+                              Validatorless.required('Senha obrigatória'),
+                              Validatorless.min(6,'Senha deve conter mínimo de 6 caracteres')
+                            ]),
+                            obscureText: true,
+                            
+                            controller: passwordEC,
+                            decoration: const InputDecoration(
+                              label: Text('Senha'),
+                              hintText: 'Senha',
+                              floatingLabelBehavior: FloatingLabelBehavior.never,
+                              labelStyle: TextStyle(color: Colors.black),
+                              hintStyle: TextStyle(color: Colors.black),
+                            ),
                           ),
-                          onPressed: () {},
-                          child: const Text(
-                            'ACESSAR',
+                          separete,
+                          const Align(
+                            alignment: Alignment.centerLeft,
+                            child: Text(
+                              'Esqueceu a senha',
+                              style: TextStyle(
+                                color: ColorsConstants.brow,
+                                fontSize: 12,
+                              ),
+                            ),
                           ),
-                        )
-                      ],
-                    ),
-                    const Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Text('Criar conta',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                          )),
-                    )
-                  ],
-                ))
-          ]),
+                          separete,
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              minimumSize:
+                                  Size.fromHeight(screenSize.height * 0.07),
+                            ),
+                            onPressed: () {},
+                            child: const Text(
+                              'ACESSAR',
+                            ),
+                          )
+                        ],
+                      ),
+                      const Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Text('Criar conta',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            )),
+                      )
+                    ],
+                  ))
+            ]),
+          ),
         ),
       ),
     );
