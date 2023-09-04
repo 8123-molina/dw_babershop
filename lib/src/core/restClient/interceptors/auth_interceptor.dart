85dd762b7e6a2fcf213b75aede576f4ad9747164
Dhyone Molina <molinaprojetos@gmail.com>
2023-09-04 09:26:25 -0300

    Verificando token valido

--- lib/src/core/restClient/interceptors/auth_interceptor.dart
+++ lib/src/core/restClient/interceptors/auth_interceptor.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:dw_barbershop/src/core/constants/local_storages_key.dart';
+import 'package:dw_barbershop/src/core/ui/widgets/barbershop_nav_global_key.dart';
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthInterceptor extends Interceptor {
@@ -18,7 +22,19 @@ class AuthInterceptor extends Interceptor {
         authHeaderKey: 'Bearer ${sp.getString(LocalStoragekeys.accessToken)}'
       });
     }
-
     handler.next(options);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final DioException(requestOptions: RequestOptions(:extra), :response) = err;
+
+    if (extra case {'DIO_AUTH_KEY': true}) {
+      if (response != null && response.statusCode == HttpStatus.forbidden) {
+        Navigator.of(BarbershopNavGlobalKey.instance.navKey.currentContext!)
+            .pushNamedAndRemoveUntil('/auth/login', (route) => false);
+      }
+    }
+    handler.reject(err);
   }
 }
